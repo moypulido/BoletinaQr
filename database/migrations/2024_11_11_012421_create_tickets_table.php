3e703b61a07a993c1820061d052a47cfffece3a1
Moises Pulido <m.npg@outlook.com>
2024-11-10 19:41:31 -0600

    Add Event and Ticket models with migrations for event management system

---
+++ database/migrations/2024_11_11_012421_create_tickets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id')->constrained()->onDelete('cascade');
+            $table->string('ticket_code')->unique();
+            $table->boolean('is_used')->default(false);
+            $table->text('qr_code');
+            $table->string('email')->nullable();
+            $table->string('name')->nullable();
+            $table->string('phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
