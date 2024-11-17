da9f64c68489323d5e234db57b231ab9187c5aa7
Moises Pulido <m.npg@outlook.com>
2024-11-17 12:22:13 -0600

    Add initial user and token migrations; remove obsolete password reset and personal access token migrations

---
+++ database/migrations/2024_11_17_180235_create_tokens_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\str;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->id();
+            $table->string('token');
+            $table->timestamps();
+        });
+
+        // Insert a single token record
+        DB::table('tokens')->insert([
+            'token' => str()->random(60),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tokens');
+    }
+};
